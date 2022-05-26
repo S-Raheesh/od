df8d6af0aa87cc571fad3183fe9b8d5944706577
S-Raheesh <80512594+S-Raheesh@users.noreply.github.com>
2022-05-26 14:55:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function StudyTable(){
+  window.location.replace("study.html");
+}
+function Bedroom(){
+    window.location.replace("bedroom.html");
+  }
+  function Space(){
+    window.location.replace("space.html");
+  }
